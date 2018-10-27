5e31fa07ea6112e3145e26b739aef9c5f854262f
Seonkyu Kim <kimsk9410@gmail.com>
2018-10-27 00:25:58 -0700

    2018-10-27

---
+++ boj/dp/11726.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    
+    int *dp = new int[n];
+    
+    dp[0] = 1;
+    dp[1] = 2;
+    
+    for(int i = 2; i < n; ++i){
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+    }
+    
+    printf("%d\n", dp[n - 1] % 10007);
+    
+    return 0;
+}
